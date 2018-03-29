505daf998081813b7962bcfae92edea0fbc85901
Yeferson Beltran Cristancho <yebeltrancr@unal.edu.co>
2018-03-29 18:21:16 -0500

    Se agrego el calculo del gini y de la entropia para un vector, en donde se realiza una simulación

--- DyYdistribucion.cpp
+++ DyYdistribucion.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <fstream>
 #include <string>
+#include <algorithm>
 #include <cmath>
 
 //Constantes
@@ -10,16 +11,25 @@ const int N = 1000; //Numero de agentes
 const int M = 1000; //Dinero total
 const int P = 10E5; //Numero de transacciones, pasos de tiempo
 const double Mprom = double(M)/N; //Dinero promedio por agente
+const int m = 20; //Estados calculo entropia
 
-//Archivos de Datos
-std::string Data = "datos.dat";
+//Vectores
+std::vector<double> V (N, Mprom); //dinero*agente inicializado con Mprom
+std::vector<double> F (m, 0); //Frecuencias
+std::vector<double> H (P, 0); //vector entropia
+
+//Archivos de Datos, ¡Cambiar nombres para cada simulacion!
+//Los archivos se reescriben si no se cambia
+std::string Distribution = "datos.dat";
 std::string Entropy = "entropia.dat";
 
-std::ofstream Datos(Data.c_str());
-std::ofstream Entropia(Entropy.c_str());
+std::ofstream DatosD(Distribution.c_str());
+std::ofstream DatosH(Entropy.c_str());
 
 //Funciones que se usan en main
 void Intercambio (std::vector<double> & V, int i, int j, double r);
+double Entropia (std::vector<double> V, std::vector<double> & F,int N, int m);
+double Gini (std::vector<double> V);
 
 int main(){
   //Generadores de numeros aleatorios
@@ -35,15 +45,22 @@ int main(){
   //Distribución numero real para delta en el intercambio
   std::uniform_real_distribution<> disr(0, 1);
 
-  //Vector de dinero por agente inicializado con el dinero promedio
-  std::vector<double> V (N, Mprom);
-
   //Simulacion
   for(int ii = 0; ii < P; ++ii){
     Intercambio(V, disi(gen1), disi(gen2), disr(gen3));
   }
 
-  for(int i = 0; i < N; ++i) Datos << V[i] << "\n";
+  //Generar el archivo con los datos distribución
+  for(int ii = 0; ii < N; ++ii) DatosD << V[ii] << "\n";
+  //Generar datos de la entropia con pasos de tiempo
+  for(int ii =0; ii < P; ++ii) DatosH << ii << "\t" << H[ii] << "\n";
+
+  double Dt = 0;
+  for (int i = 0; i < N; ++i) Dt += V[i];
+  std::cout << "DineroT: " << Dt << "\n";
+  std::cout << "Entropia: " << Entropia(V, F, N, m) << "\n";
+  std::cout << "Gini: " << Gini(V) << "\n";
+
 
   return 0;
 }
@@ -58,4 +75,78 @@ void Intercambio (std::vector<double> & V, int i, int j, double r){
     V[i] += delta;
     V[j] -= delta;
   }
+}
+
+double Gini (std::vector<double> Vp){
+
+  std::vector<double> V = Vp;
+
+  //Organizar vector
+  std::sort(V.begin(), V.end());
+
+  //Población acumulada Xi+1 - Xi
+  int Pt = V.size(); //tamaño de la población
+
+  std::vector<double> Xt ;
+  for (int i = 0; i < Pt; ++i)
+    Xt.push_back(1.0/Pt);
+
+  //Dinero acumulado
+
+  double Dt = 0.0; //Dinero total
+  for (int i = 0; i < Pt; ++i) Dt += V[i]; //suma componentes
+
+  std::vector<double> Y = V; //normalización
+  for (int i = 0; i < Pt; ++i) Y[i] /= Dt; 
+
+  std::vector<double> AY = Y; //acumulado
+  for (int i = 0; i < Pt; ++i){
+    if (i == 0) AY[i] = Y[i];
+    else AY[i]= AY[i-1] + Y[i];
+  }
+
+  std::vector<double> Yt = AY; // Yi+1 + Yi
+  for (int i = 0; i < Pt; ++i){
+    if (i==1) Yt[i] = AY[i];
+    else Yt[i] = AY[i-1] + AY[i];
+  }
+
+  double L = 0.0;
+  for (int i = 0; i < Pt; ++i) L += Yt[i]*Xt[i];
+
+  return 1 - L;
+}
+
+double Entropia (std::vector<double> V, std::vector<double> & F,int N, int m){
+
+  //Maximo
+  double Max = 0.0;
+  for(int ii = 0; ii < N; ++ii){
+    if (Max < V[ii]) Max = V[ii];
+  }
+
+  //delta
+  double Delta = Max/m;
+
+  //Contando agentes en el intervalo
+  for(int i = 0; i < N; ++i){
+    for(int j = 0; j < m; ++j){
+      if (V[i] > Delta*j && V[i] <= (j+1)*Delta ){
+        F[j] = F[j] + 1;
+        break;
+      }
+    }
+  }
+
+  //Probabilidad
+  for(int i = 0; i < m; ++i) F[i] = F[i]/N;
+
+  //Entropia
+  double H = 0.0;
+  for (int i = 0; i < m ; ++i){
+    if (F[i] != 0){
+      H += (-1)*F[i]*log(F[i]);
+    }
+  }
+  return H;
 }
